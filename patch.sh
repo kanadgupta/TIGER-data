8fc69eea953610edad2e5e689d29018675f161ca
marc tobias <mtmail@gmx.net>
2025-04-16 17:18:20 +0200

    88914 is file 32003 likely a typo

--- patch.sh
+++ patch.sh
@@ -1,5 +1,8 @@
 #/bin/bash
 
 # The 1..45 line is way off
-grep -v '45;1;all;Woodfall Rd;Middlesex;MA;02478' 25017.csv > 25017.csv.tmp
+grep -v '45;1;all;Woodfall Rd;Middlesex;MA;02478' 25017.csv >25017.csv.tmp
 mv 25017.csv.tmp 25017.csv
+
+# 88914 is a typo, should be 89141 like the other road sections
+sed -i 's/Olympia Ridge Dr;Clark;NV;88914/Olympia Ridge Dr;Clark;NV;89141/' 32003.csv
